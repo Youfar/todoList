68e0ddc2f6f1125bb6011b08fb931159ecc0b445
Youfar <wakafar@gmail.com>
2016-08-20 16:04:58 +0900

    0.2 add new items to todolist

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,33 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all
   end
+
+  def new
+  	@todo = Todo.new
+  end
+
+  def create
+  	@todo = Todo.create(todo_params)
+  	redirect_to todos_path
+  end
+
+  #def edit
+  #  @todo = Todo.find(params[:id])
+  #end
+
+  #def update
+  #  @todo = Todo.find(params[:id])
+  #  @todo.update
+  #end
+
+  #def destroy 
+  #  @todo = Todo.find(params[:id])
+  #  @todo.destroy
+  #end
+
+  private
+  def todo_params
+  	params.require(:todo).permit(:item)
+  end
+
 end
